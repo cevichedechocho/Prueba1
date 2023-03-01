39fff0e975b56eab472eb833ca84ca5f35741da1
cevichedechocho <121741496+cevichedechocho@users.noreply.github.com>
2023-03-01 14:31:25 -0500

    Nuevo archivo Cocina

---
+++ Cocina.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ 
+  cout <<"Pushhhhhh\n\n\n";
+  
+  cout <<"Para ver el proceso de actualizacion de los archivos en la computadora";
+  
+  // Hola xd
+  
+}
